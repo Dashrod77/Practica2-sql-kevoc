a85d3f7988069fde21eee916bb11a317488ab847
Elias <Ozieleliasr@gmail.com>
2026-09-13 12:15:00 -0600

    [feat]Creacion del TRIGGER 2-UPDATE

--- TR01.sql
+++ TR01.sql
@@ -17,7 +17,8 @@ CREATE TRIGGER TR01_UPDATE
 AFTER UPDATE ON equipo
 FOR EACH ROW
 BEGIN
-UPDATE equipo
+INSERT INTO auditoria (tabla_afectada, operacion, registro_id, valor_anterior, valor_nuevo, descripcion)
+VALUES ('equipo', 'UPDATE', NEW.equipo_id, JSON_OBJECT('equipo_id', OLD.equipo_id, 'numero_serie', OLD.numero_serie, 'activo', OLD.activo), JSON_OBJECT('equipo_id', NEW.equipo_id, 'numero_serie', NEW.numero_serie, 'activo', NEW.activo), 'se a actualizado un equipo');
 END $$
 DELIMITER ;
 
